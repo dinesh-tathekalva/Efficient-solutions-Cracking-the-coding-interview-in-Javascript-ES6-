a7c5a588b9a75a677d3c6a51feac54cca94dc2e1
Mickey <mickey@Mickeys-MacBook-Pro.local>
2020-05-22 00:01:45 -0500

    Problem added  WIP

---
+++ MatrixRotation.js
@@ -0,0 +1,12 @@
+var MatRot = () => {
+    var arr = [[1,2,3], [4,5,6], [7,8,9]] 
+    for(let i=0;i<arr.length; i++){
+      for(let j=0;j<i;j++){
+         const temp = arr[i][j]
+         arr[i][j] = arr[j][i]
+         arr[j][i] = temp
+         }
+       }
+     return console.log(arr)
+   }
+   MatRot()
