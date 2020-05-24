6f245dabc6e351e74432c1ce37a033e92b6c7ece
Mickey <mickey@Mickeys-MacBook-Pro.local>
2020-05-23 21:59:06 -0500

    Problem added

---
+++ ArrayDupeWithMaps.js
@@ -0,0 +1,15 @@
+var countArray = () =>{
+    var arr = [1,2,3,2,4,1,5,6,7,4]
+    var hsmap = new Map()
+    for(let i=0; i<arr.length; i++){
+      if(hsmap.has(arr[i])){
+        hsmap.set(arr[i], hsmap.get(arr[i])+1)
+      }
+      else{
+        hsmap.set(arr[i], 1)
+      }
+    }
+    return console.log(hsmap)
+  }
+  
+  countArray()
